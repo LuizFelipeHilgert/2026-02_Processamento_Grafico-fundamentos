2bbcc5838b8ee16f4782e8db1edcf79ec1c9b897
LuizFelipeHilgert <159270455+LuizFelipeHilgert@users.noreply.github.com>
2026-08-06 15:56:43 -0300

    Ajusta OpenGL 4.1 para macOS

--- src/Exemplos/HelloTriangle/HelloTriangle.cpp
+++ src/Exemplos/HelloTriangle/HelloTriangle.cpp
@@ -47,7 +47,7 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 
 // Código fonte do Vertex Shader (em GLSL): ainda hardcoded
 const GLchar *vertexShaderSource = R"glsl(
- #version 400
+ #version 410 core
  layout (location = 0) in vec3 position;
  void main()
  {
@@ -57,7 +57,7 @@ const GLchar *vertexShaderSource = R"glsl(
 
 // Código fonte do Fragment Shader (em GLSL): ainda hardcoded
 const GLchar *fragmentShaderSource = R"glsl(
- #version 400
+ #version 410 core
  uniform vec4 inputColor;
  out vec4 color;
  void main()
@@ -76,10 +76,10 @@ int main()
 	// Você deve adaptar para a versão do OpenGL suportada por sua placa
 	// Sugestão: comente essas linhas de código para desobrir a versão e
 	// depois atualize (por exemplo: 4.5 com 4 e 5)
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Ativa a suavização de serrilhado (MSAA) com 8 amostras por pixel
 	// glfwWindowHint(GLFW_SAMPLES, 8);
